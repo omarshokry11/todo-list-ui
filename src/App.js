550968e7b2ad00f8c8fe5ee99dd48a615434fcd5
omarshokry11 <omarshokry238@gmail.com>
2023-11-03 11:47:21 +0200

    Upload Project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState } from "react";
+import "./Style/App.scss";
 
-function App() {
+export default function App() {
+  const [todos, setTodos] = useState([]);
+  const inputRef = useRef();
+  const handleAddTodos = () => {
+    const text = inputRef.current.value;
+    const newItem = { completed: false, text };
+    setTodos([...todos, newItem]);
+    inputRef.current.value = "";
+  };
+  const handleItemDone = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].completed = !newTodos[index].completed;
+    setTodos(newTodos);
+  };
+  const handleDeleteItem = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-app-container">
+      <div className="main-app-content">
+        <h1>Todo list</h1>
+        <ul>
+          {todos.map(({ text, completed }, index) => (
+            <div className="item" key={index}>
+              <li onClick={() => handleItemDone(index)}>
+                <p style={{ width: "4%" }}>
+                  {completed ? (
+                    <span style={{ marginRight: "10px" }}>✔️</span>
+                  ) : (
+                    <span style={{ marginRight: "10px" }}>❌</span>
+                  )}
+                </p>
+                <button className="btn-text">{text}</button>
+              </li>
+              <span
+                className="btn-delete"
+                onClick={() => handleDeleteItem(index)}
+              >
+                Delete
+              </span>
+            </div>
+          ))}
+        </ul>
+        <input ref={inputRef} type="text" placeholder="Enter the todo...." />
+        <button className="btn-add" onClick={handleAddTodos}>
+          Add Todo
+        </button>
+      </div>
     </div>
   );
 }
-
-export default App;
